f818901a5e844b3e63eb21e984de709c1c890cb9
Luiz Cláudio <lc.danella.dev@gmail.com>
2021-05-20 19:08:47 -0300

    Reestruturado

---
+++ index.js
@@ -0,0 +1,15 @@
+const imagens = [
+  { nome: "image-john-jpg", key: 0 },
+  { nome: "image-tanya-jpg", key: 1 },
+];
+
+document.querySelector("body").addEventListener("click", (e) => {
+  console.log(e.target.classList.contains("left"));
+  if(e.target.classList.contains("left")){
+
+  };
+});
+
+function mudaFoto() {
+  
+}
